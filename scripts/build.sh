cada7683685c40944bacbf7d6ac833b8e0d2b3dd
Matt Welke <mattwelke@gmail.com>
2021-10-17 17:38:16 -0400

    Switch to Docker Hub acount owextendedruntimes.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ SECONDS_SINCE_EPOCH=$(printf '%(%s)T\n' -1)
 
 TAG=$SECONDS_SINCE_EPOCH
 
-docker build -t mwelke/apache-openwhisk-runtime-java-17:$TAG .
+docker build -t owextendedruntimes/java-17:$TAG .
