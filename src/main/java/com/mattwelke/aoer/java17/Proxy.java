1fd24fc25750b3b32ccc1f45848b2640b8fc4aab
Matt Welke <mattwelke@gmail.com>
2021-10-17 23:57:26 -0400

    Add build script for experiment.

--- src/main/java/com/mattwelke/aoer/java17/Proxy.java
+++ src/main/java/com/mattwelke/aoer/java17/Proxy.java
@@ -9,8 +9,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
